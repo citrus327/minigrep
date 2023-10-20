56ec328f6c86a862f86f9f2d0bfa56f107ae1156
citrus327 <citrus327@outlook.com>
2023-10-20 17:36:12 +0800

    feat: lint code

--- src/engine.rs
+++ src/engine.rs
@@ -4,33 +4,41 @@ use crate::{search_param::SearchParam, search_result::SearchResult, utils};
 
 #[derive(Debug)]
 pub struct Engine<'a> {
-    config: &'a SearchParam,
+    search_param: &'a SearchParam,
     results: Vec<SearchResult>,
 }
 
 impl<'a> Engine<'a> {
     pub fn from(search_param: &'a SearchParam) -> Self {
         Self {
-            config: search_param,
+            search_param: search_param,
             results: Vec::new(),
         }
     }
     fn read(&self, file_path: &str) -> String {
         let content = fs::read_to_string(file_path);
-        let content = content.unwrap_or_else(|e| utils::die(&e));
-        content
+        content.unwrap_or_else(|e| utils::die(&e))
     }
-    pub fn search(&mut self) {
-        let content = self.read(&self.config.file_path);
+
+    pub fn search(&mut self) -> &Self {
+        let content = self.read(&self.search_param.file_path);
 
         let mut i = 0;
-        for line in content.lines() {
-            if line.contains(&self.config.search_string.as_str()) {
+        for (_, line) in content.lines().enumerate() {
+            if line.contains(self.search_param.search_string.as_str()) {
                 let result: SearchResult = SearchResult::from(i, line.to_string());
                 self.results.push(result)
             }
 
             i += 1;
+        }
+        self
+    }
+
+    pub fn pretty_print(&self) {
+        println!("Found {} occurrences", &self.results.len());
+        for element in &self.results {
+            println!("{} in line {}", element.content, element.line_num)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ mod utils;
 fn main() {
     let search_param = search_param::SearchParam::from_args();
     let mut engine = engine::Engine::from(&search_param);
-    engine.search();
-    dbg!(engine);
+    engine.search().pretty_print();
+    // dbg!(engine);
 }
